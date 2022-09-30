03f7211eaed22f674aaf4ebcdd167beeac01f1a7
Fernando E. Zimerman <fernando.ezimerman@gmail.com>
2022-09-30 11:31:07 -0300

    Add files via upload

--- siteMercado.py
+++ siteMercado.py
@@ -12,7 +12,7 @@ t = 10
 
 def main():
     service = Service(ChromeDriverManager().install())
-    names = []
+    mercadosList = []
     prices = []
     namesList = []
     pricesList = []
@@ -20,26 +20,50 @@ def main():
     driver = webdriver.Chrome(service=service)
     driver.get("https://www.sitemercado.com.br/")
     driver.find_element(By.XPATH, '/html/body/app-root/app-cookie-notice/div/button').click()
-    #digitar o cep
-    driver.find_element(By.TAG_NAME, 'input').send_keys("35400000" + Keys.RETURN)
-    time.sleep(t)
-    #digitar o endereço
-    driver.find_element(By.ID, 'streetAddress').send_keys("Rua João Fernandes Vieira")
-    driver.find_element(By.ID, 'neighborhood').send_keys("Bauxita")
-    driver.find_element(By.ID, 'numberAddress').send_keys("256")
-    driver.find_element(By.XPATH, '/html/body/ngb-modal-window/div/div/app-filter-stores-modal/div/div/app-address-form/form/div[6]/button').click()
-    time.sleep(t)
-    #confirmar localização
-    driver.find_element(By.XPATH, '/html/body/ngb-modal-window/div/div/app-filter-stores-modal/div/div/app-address-map/div/div/div[3]/button[2]').click()
-    time.sleep(t)
-    driver.find_element(By.XPATH, '/html/body/ngb-modal-window/div/div/app-filter-stores-modal/div/div/app-store-list/div/app-filter/div/div[1]/button[2]').click()
-    mercados = driver.find_elements(By.CLASS_NAME, 'item ng-star-inserted')
-    time.sleep(t)
     
-    for mercado in mercados:
-        mercado.click()
-        time.sleep(t)
-        '''
+    try:
+        
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-cookie-notice/div/button')))
+        element.click()
+
+        #digitar o cep
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'input'))).send_keys("35400000" + Keys.RETURN)
+        #element.send_keys("35400000" + Keys.RETURN)
+        #digitar o endereço
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'streetAddress'))).send_keys("Rua João Fernandes Vieira")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'neighborhood'))).send_keys("Bauxita")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'numberAddress'))).send_keys("256")
+    
+    except:
+        driver.quit()
+
+    try:
+        #confirmar insercao de dados
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'btnAdd')))
+        element.click()
+
+        #confirmar localização
+        confirmLocation = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/ngb-modal-window/div/div/app-filter-stores-modal/div/div/app-address-map/div/div/div[3]/button[2]')))
+        confirmLocation.click()
+    except:
+        driver.quit()
+
+    try:
+        #btn retirada
+        retirada = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/ngb-modal-window/div/div/app-filter-stores-modal/div/div/app-store-list/div/app-filter/div/div[1]/button[2]')))
+        retirada.click()
+        
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'item ng-star-inserted')))
+        mercadosList = driver.find_elements(By.CLASS_NAME, 'item ng-star-inserted')
+
+        for m in mercadosList:
+            print(m)
+    except:
+        driver.quit()    
+
+    driver.quit()
+    
+    '''
         time.sleep(t)
         mercado.find_element(By.CLASS_NAME, 'item-menu active').click()
         secoes = mercado.find_elements(By.CLASS_NAME, 'content')
